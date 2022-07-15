499914997a58d23922ee8c8e91364d1bae83fd41
David Erb <david.erb@diamond.ac.uk>
2022-07-15 11:26:14 +0100

    5.0.7 includes static

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 def main():
     name = "dls-logform"
-    version = "5.0.5"
+    version = "5.0.7"
     description = "Logging formatter."
     author = "David Erb"
     author_email = "david.erb@diamond.ac.uk"
