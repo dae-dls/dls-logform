2fb2627261be75f4d4ebc3413aee8b94afd49d79
David Erb <david.erb@diamond.ac.uk>
2022-07-15 12:43:20 +0100

    5.0.17 removes github_versions theme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 def main():
     name = "dls-logform"
-    version = "5.0.13"
+    version = "5.0.17"
     description = "Logging formatter."
     author = "David Erb"
     author_email = "david.erb@diamond.ac.uk"
